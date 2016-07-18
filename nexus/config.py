568d499c03b9540b6da3a9435f0ac86fa6896973
xiaogzho <zhou16386@163.com>
2016-07-18 16:42:10 +0800

    test

--- nexus/config.py
+++ nexus/config.py
@@ -4,3 +4,5 @@ class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/nexus.sqlite' % APPLICATION_DIR
+
+#this is for test
